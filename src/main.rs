53cde29120a75bd5b828543fe3457615dd89abdf
Rudi Blaha Svartveit <25234080+rudsvar@users.noreply.github.com>
2024-03-02 19:44:06 +0100

    Almost got fibonacci working

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ pub enum Value {
     String(String),
     Type(Type),
     Record(Record<Value>),
+    Fun(Fun),
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
@@ -66,7 +67,7 @@ impl<T, E> Record<Result<T, E>> {
     }
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub enum Expr {
     // Bool
     Bool(bool),
@@ -74,6 +75,7 @@ pub enum Expr {
     Int(i128),
     Lt(Box<Expr>, Box<Expr>),
     Add(Box<Expr>, Box<Expr>),
+    Sub(Box<Expr>, Box<Expr>),
     // String
     String(String),
     // Var
@@ -83,6 +85,9 @@ pub enum Expr {
     FieldAccess(Box<Expr>, String),
     // Control flow
     If(Box<Expr>, Box<Expr>, Box<Expr>),
+    // Functions
+    Fun(String, Fun),
+    FunCall(String, Vec<Expr>),
 }
 
 #[derive(Clone, Debug, PartialEq, Eq)]
@@ -92,12 +97,21 @@ pub enum Type {
     String,
     Type,
     Record(Record<Type>),
+    Fun(Vec<Type>, Box<Type>),
+}
+
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct Fun {
+    params: Vec<(String, String)>,
+    return_ty: String,
+    body: Box<Expr>,
 }
 
 #[derive(Debug)]
 pub enum Stmt {
-    VarDecl(String, Option<String>, Expr),
+    VarDef(String, Option<String>, Expr),
     TypeDef(String, Type),
+    FunDef(String, Fun),
     PrintLn(Expr),
 }
 
@@ -109,6 +123,8 @@ pub enum Error {
     NoFields(String),
     NoSuchField(String),
     TypeError { expected: Type, actual: Type },
+    WrongNumberOfArgs { expected: usize, actual: usize },
+    Custom(String),
 }
 
 #[derive(Debug)]
@@ -166,6 +182,10 @@ pub fn eval(expr: Expr, env: &Env) -> Result<Value, Error> {
             (Value::Int(a), Value::Int(b)) => Value::Int(a + b),
             _ => panic!("Add type error"),
         },
+        Expr::Sub(e1, e2) => match (eval(*e1, env)?, eval(*e2, env)?) {
+            (Value::Int(a), Value::Int(b)) => Value::Int(a - b),
+            _ => panic!("Sub type error"),
+        },
         Expr::String(s) => Value::String(s),
         Expr::Var(v) => env.get(&v)?.1,
         Expr::Record(r) => {
@@ -185,6 +205,32 @@ pub fn eval(expr: Expr, env: &Env) -> Result<Value, Error> {
             Value::Bool(false) => eval(*e2, env)?,
             _ => panic!("If type error"),
         },
+        // Nothing to evaluate until called
+        Expr::Fun(_, f) => Value::Fun(f),
+        // Evaluate arguments, evaluate fun body in env with parameters
+        Expr::FunCall(f_name, args) => {
+            tracing::info!("Evaluating function call {}({:?})", f_name, args);
+            // Look up function to call
+            let (_, f) = env.get(&f_name)?;
+            // Check that it is a function
+            let Value::Fun(f) = f else {
+                return Err(Error::Custom("Expected function".to_string()));
+            };
+            tracing::info!("{f_name} is a function");
+            // Prepare function env
+            let mut fun_env = Env::default();
+            for ((param_name, param_ty_name), arg) in f.params.into_iter().zip(args) {
+                let Value::Type(param_ty) = env.get(&param_ty_name)?.1 else {
+                    return Err(Error::Custom(format!("{param_ty_name} is not a type")));
+                };
+                let arg_ty = type_of(&arg, env)?;
+                assert_type(&param_ty, &arg_ty)?;
+                let arg_val = eval(arg, env)?;
+                fun_env.put(param_name, param_ty, arg_val)?;
+            }
+            // Evaluate function body in new env
+            eval(*f.body, &fun_env)?
+        }
     })
 }
 
@@ -198,6 +244,11 @@ fn type_of(expr: &Expr, env: &Env) -> Result<Type, Error> {
             Type::Bool
         }
         Expr::Add(e1, e2) => {
+            assert_type(&Type::Int, &type_of(e1, env)?)?;
+            assert_type(&Type::Int, &type_of(e2, env)?)?;
+            Type::Int
+        }
+        Expr::Sub(e1, e2) => {
             assert_type(&Type::Int, &type_of(e1, env)?)?;
             assert_type(&Type::Int, &type_of(e2, env)?)?;
             Type::Int
@@ -229,6 +280,81 @@ fn type_of(expr: &Expr, env: &Env) -> Result<Type, Error> {
                 });
             }
             e1_ty
+        }
+        Expr::Fun(f_name, f) => {
+            tracing::info!("Type checking function");
+            let param_types: Vec<Type> = f
+                .params
+                .iter()
+                .map(|(_, param_ty_name)| {
+                    let Value::Type(param_ty) = env.get(param_ty_name)?.1 else {
+                        return Err(Error::Custom(format!(
+                            "Expected {param_ty_name} to be a type"
+                        )));
+                    };
+                    Ok(param_ty)
+                })
+                .collect::<Result<Vec<Type>, Error>>()?;
+            tracing::info!("Parameter types: {param_types:?}");
+            let Value::Type(return_type) = env.get(&f.return_ty)?.1 else {
+                return Err(Error::Custom(format!(
+                    "Expected return type {} to be a type",
+                    f.return_ty
+                )));
+            };
+            tracing::info!("Return type: {return_type:?}");
+            // Assume params defined with correct types.
+            let mut fun_env = Env::default();
+            for ((param_name, _), param_ty) in f.params.iter().zip(&param_types) {
+                // TODO: Placeholder value is unnecessary for type checking.
+                fun_env.put(param_name.clone(), param_ty.clone(), Value::Int(0))?;
+            }
+            // Put function itself in env to enable recursion
+            fun_env.put(
+                f_name.clone(),
+                Type::Fun(param_types.clone(), Box::new(return_type.clone())),
+                Value::Fun(f.clone()),
+            )?;
+            // Type check in new env
+            let body_type = type_of(&f.body, &fun_env)?;
+            tracing::info!("Body type: {:?}", body_type);
+            assert_type(&return_type, &body_type)?;
+            Type::Fun(param_types, Box::new(return_type))
+        }
+        Expr::FunCall(f_name, args) => {
+            tracing::info!("Type checking function call to {f_name}");
+            // Look up function to call
+            let (_, f) = env.get(f_name)?;
+            // Check that it is a function
+            let Value::Fun(f) = f else {
+                return Err(Error::Custom("Expected function".to_string()));
+            };
+            tracing::info!("It's a function at least");
+            // Check number of args.
+            if args.len() != f.params.len() {
+                return Err(Error::WrongNumberOfArgs {
+                    expected: f.params.len(),
+                    actual: args.len(),
+                });
+            }
+            tracing::info!("Called with the correct number of arguments");
+            // Arguments must satisfy parameter types
+            // TODO: Custom type definitions will not be included. Separate env or keep?
+            for ((param_name, param_ty_name), arg) in f.params.into_iter().zip(args) {
+                let Value::Type(param_ty) = env.get(&param_ty_name)?.1 else {
+                    return Err(Error::Custom(format!("{param_ty_name} is not a type")));
+                };
+                tracing::info!("Parameter {param_name} has type {param_ty:?}");
+                let arg_ty = type_of(arg, env)?;
+                tracing::info!("Corresponding argument has type {arg_ty:?}");
+                assert_type(&param_ty, &arg_ty)?;
+            }
+            // Type of function call is same as return type
+            let Value::Type(return_ty) = env.get(&f.return_ty)?.1 else {
+                return Err(Error::Custom(format!("{} is not a type", f.return_ty)));
+            };
+            tracing::info!("Return type is {return_ty:?}");
+            return_ty
         }
     };
     Ok(ty)
@@ -268,7 +394,7 @@ fn assert_type(expected: &Type, actual: &Type) -> Result<(), Error> {
 pub fn step(stmt: Stmt, env: &mut Env) -> Result<(), Error> {
     match stmt {
         // If specified, `e` must satisfy type `ty`.
-        Stmt::VarDecl(name, required_ty, e) => {
+        Stmt::VarDef(name, required_ty, e) => {
             let real_type = type_of(&e, env)?;
             if let Some(required_ty) = required_ty {
                 match env.get(&required_ty)? {
@@ -281,12 +407,19 @@ pub fn step(stmt: Stmt, env: &mut Env) -> Result<(), Error> {
             env.put(name, real_type, eval(e, env)?)?;
         }
         Stmt::TypeDef(name, ty) => env.put(name, Type::Type, Value::Type(ty))?,
+        Stmt::FunDef(name, f) => {
+            tracing::info!("Defining function {name}");
+            let ty = type_of(&Expr::Fun(name.clone(), f.clone()), env)?;
+            tracing::info!("It got type {ty:?}");
+            env.put(name, ty, Value::Fun(f))?;
+        }
         Stmt::PrintLn(e) => match eval(e, env)? {
             Value::Bool(b) => println!("{b}"),
             Value::Int(i) => println!("{i}"),
             Value::String(s) => println!("{s}"),
             Value::Type(t) => println!("{t:?}"),
             Value::Record(o) => println!("{o:?}"),
+            Value::Fun(f) => println!("{:?} -> {:?}", f.params, f.return_ty),
         },
     }
     Ok(())
@@ -307,39 +440,46 @@ fn init_logging() {
 fn main() {
     init_logging();
 
+    let n: i128 = std::env::args()
+        .nth(1)
+        .and_then(|s| s.parse().ok())
+        .unwrap_or(0);
+
     let program = vec![
-        Stmt::TypeDef(
-            "Foo".to_string(),
-            Type::Record(Record {
-                fields: {
-                    let mut fields = BTreeMap::new();
-                    fields.insert("i".to_string(), Type::Int);
-                    fields.insert("s".to_string(), Type::String);
-                    fields
-                },
-            }),
+        Stmt::FunDef(
+            "fib".to_string(),
+            Fun {
+                params: vec![("n".to_string(), "Int".to_string())],
+                return_ty: "Int".to_string(),
+                body: Box::new(Expr::If(
+                    // If n < 2
+                    Box::new(Expr::Lt(
+                        Box::new(Expr::Var("n".to_string())),
+                        Box::new(Expr::Int(2)),
+                    )),
+                    // evaluate to n
+                    Box::new(Expr::Var("n".to_string())),
+                    // else evaluate to fib(n-1) + fib(n-1)
+                    Box::new(Expr::Add(
+                        Box::new(Expr::FunCall(
+                            "fib".to_string(),
+                            vec![Expr::Sub(
+                                Box::new(Expr::Var("n".to_string())),
+                                Box::new(Expr::Int(1)),
+                            )],
+                        )),
+                        Box::new(Expr::FunCall(
+                            "fib".to_string(),
+                            vec![Expr::Sub(
+                                Box::new(Expr::Var("n".to_string())),
+                                Box::new(Expr::Int(2)),
+                            )],
+                        )),
+                    )),
+                )),
+            },
         ),
-        Stmt::VarDecl(
-            "foo".to_string(),
-            Some("Foo".to_string()),
-            Expr::Record(Record {
-                fields: vec![
-                    ("i".to_string(), Expr::Int(5)),
-                    ("s".to_string(), Expr::String("hello".to_string())),
-                ]
-                .into_iter()
-                .collect(),
-            }),
-        ),
-        Stmt::PrintLn(Expr::Var("foo".to_string())),
-        Stmt::PrintLn(Expr::FieldAccess(
-            Box::new(Expr::Var("foo".to_string())),
-            "i".to_string(),
-        )),
-        Stmt::PrintLn(Expr::FieldAccess(
-            Box::new(Expr::Var("foo".to_string())),
-            "s".to_string(),
-        )),
+        Stmt::PrintLn(Expr::FunCall("fib".to_string(), vec![Expr::Int(n)])),
     ];
     let mut env = Env::default();
     if let Err(e) = exec(program, &mut env) {
@@ -370,7 +510,7 @@ mod tests {
     fn variable_declaration_leaves_var_in_env() {
         init_logging();
         let mut env = Env::default();
-        let stmt = Stmt::VarDecl("a".to_string(), None, Expr::Int(0));
+        let stmt = Stmt::VarDef("a".to_string(), None, Expr::Int(0));
         step(stmt, &mut env).unwrap();
         assert_eq!((Type::Int, Value::Int(0)), env.get("a").unwrap())
     }
@@ -379,7 +519,7 @@ mod tests {
     fn variable_declaration_with_wrong_type_fails() {
         init_logging();
         let mut env = Env::default();
-        let stmt = Stmt::VarDecl("a".to_string(), Some("String".to_string()), Expr::Int(0));
+        let stmt = Stmt::VarDef("a".to_string(), Some("String".to_string()), Expr::Int(0));
         assert_eq!(
             Err(Error::TypeError {
                 expected: Type::String,
@@ -393,7 +533,7 @@ mod tests {
     fn variable_usage_finds_var_in_env() {
         init_logging();
         let stmts = vec![
-            Stmt::VarDecl("a".to_string(), None, Expr::Int(0)),
+            Stmt::VarDef("a".to_string(), None, Expr::Int(0)),
             Stmt::PrintLn(Expr::Var("a".to_string())),
         ];
         exec(stmts, &mut Env::default()).unwrap();
@@ -423,7 +563,7 @@ mod tests {
                     },
                 }),
             ),
-            Stmt::VarDecl(
+            Stmt::VarDef(
                 "foo".to_string(),
                 Some("Foo".to_string()),
                 Expr::Record(Record {
@@ -457,7 +597,7 @@ mod tests {
                     },
                 }),
             ),
-            Stmt::VarDecl(
+            Stmt::VarDef(
                 "foo".to_string(),
                 Some("Foo".to_string()),
                 Expr::Record(Record {
@@ -506,7 +646,7 @@ mod tests {
                     },
                 }),
             ),
-            Stmt::VarDecl(
+            Stmt::VarDef(
                 "foo".to_string(),
                 Some("Foo".to_string()),
                 Expr::Record(Record {
